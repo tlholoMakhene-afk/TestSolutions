86acc72afbd2f6fc34969c75a3fc77a7ffe9a22e
Tlholo <58808719+tlholoMakhene-afk@users.noreply.github.com>
2020-11-30 21:36:28 +0100

    Change

--- QUestions/Question4/Program.cs
+++ QUestions/Question4/Program.cs
@@ -2,34 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace QUestions.Question4
+namespace Functions
 {
     class Program
     {
         public static int CalSum(List<string> list)
         {
-            
+
             if (list.Count == 1)
             {
-                 if (Int32.TryParse(list[0], out int result))
-                    return Convert.ToInt32(result);
+                if (Int32.TryParse(list[0], out int result))
+                    return result;
 
             }
             else
             {
                 if (Int32.TryParse(list[0], out int result))
-                    return Convert.ToInt32(list[0]) + CalSum(list.Skip(1).ToList());
+                    return result + CalSum(list.Skip(1).ToList());
             }
             return CalSum(list.Skip(1).ToList());
         }
         static void Main(string[] args)
         {
-            var list2 = new List<string>() {  "100", "geek", "200", "400", "for", "101", "2018", "64", "74", "geeks", "27", "7810"};
-            Console.WriteLine($"Sum of Intergers in String List{CalSum(list2)}" ); 
-
-
+            var list2 = new List<string>() { "100", "geek", "200", "400", "for", "101", "2018", "64", "74", "geeks", "27", "7810" };
+            Console.WriteLine($"Sum of Intergers in String List{CalSum(list2)}");
             Console.ReadKey();
         }
     }
 }
-
