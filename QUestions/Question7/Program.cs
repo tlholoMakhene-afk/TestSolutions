fc03f9e68e9e63b7d2b99c829006399b35226c25
Tlholo <58808719+tlholoMakhene-afk@users.noreply.github.com>
2020-11-30 22:01:44 +0100

    Fix

--- QUestions/Question7/Program.cs
+++ QUestions/Question7/Program.cs
@@ -9,63 +9,37 @@ namespace QUestions.Question7
 {
     class Program
     {
-        public static void editBytes(byte[] arr)
-        {
-
-
-            int leftShiftAmount = 0;
-            int i = 7;//We trancate at every 7th byte in the byte[]
-            for (i = 7; i < arr.Length; i += 7)
-            {
-                memmove(arr[i] - leftShiftAmount, arr[i + 1], sizeof(byte) * 4);  //memmove() is similar to memcpy() as it also copies data from a source to destination. this is a c++ method no c# equivalent
-                leftShiftAmount++;
-            }
-            int clearIndex = i - 7 - leftShiftAmount + 4;
-            for (clearIndex = i - 7 - leftShiftAmount + 4; clearIndex < arr.Length; clearIndex++)
-            {//clear the garbage at the end, or you can just truncate up to this point
-                arr[clearIndex] = 0;//clears occurs here to the byte[] at the clearIndex index
-            }
-        }
-
-        private static void memmove(int v1, byte v2, int v3)
-        {
-            throw new NotImplementedException();
-        }
-
 
         public static void Main()
         {
-            // Specify a file to read from and to create.
-            string pathSource = @"c:\tests\newfile.txt";
-            string pathNew = @"c:\tests\newfile.txt";
             try
             {
-                using (FileStream fsSource = new FileStream(pathSource,
-                    FileMode.Open, FileAccess.Read))
-                {
-                    // Read the source file into a byte array.
-                    byte[] bytes = new byte[fsSource.Length];
-                    int numBytesToRead = (int)fsSource.Length;
-                    int numBytesRead = 0;
-                    editBytes(bytes); // this is where the magic happens
-                    while (numBytesToRead > 0)
+                    string fileName = @"C:\Users\pxyz6n\source\repos\Functions\Functions\Questions.bin";
+                    List<byte> list;
+                    using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+
                     {
-                        // Read may return anything from 0 to numBytesToRead.
-                        int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
-                        // Break when the end of the file is reached.
-                        if (n == 0)
-                            break;
-                        numBytesRead += n;
-                        numBytesToRead -= n;
+                        byte[] arrfile = new byte[fs.Length];
+                        fs.Read(arrfile, 0, arrfile.Length);
+                        list = new List<byte>(arrfile);
                     }
-                    numBytesToRead = bytes.Length;
-                    // Write the byte array to the other FileStream.
-                    using (FileStream fsNew = new FileStream(pathNew,
-                        FileMode.Create, FileAccess.Write))
+
+                    for (int i = 0; i < list.Count(); i++)
                     {
-                        fsNew.Write(bytes, 0, numBytesToRead);
+                        if (i == 7)
+                        {
+                            list.RemoveAt(7);
+                        }
+
                     }
-                }
+
+                    byte[] resultArr = list.ToArray();
+
+                    using (var fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        fs.Write(resultArr, 0, resultArr.Length);
+
+                    }
             }
             catch (FileNotFoundException ioEx)
             {
