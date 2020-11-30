e674d5fcff53a196c108d440ddd95b7daf1053f9
Tlholo <58808719+tlholoMakhene-afk@users.noreply.github.com>
2020-11-30 21:34:38 +0100

    Update Program.cs

--- QUestions/Question4/Program.cs
+++ QUestions/Question4/Program.cs
@@ -1,35 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QUestions.Question4
 {
     class Program
     {
-        public static int GetsumIntArray(string[] arr, int count = 0)
+        public static int CalSum(List<string> list)
         {
-            var value = 0;
-            if (arr[count] != null)
+            
+            if (list.Count == 1)
             {
-                if (arr[count].GetType() == typeof(int))
-                {  // type check    
-                    value = int.Parse(arr[count]);
-                }
-                return (value + GetsumIntArray(arr, count + 1)); //Throws an index out of  range exception
+                 if (Int32.TryParse(list[0], out int result))
+                    return Convert.ToInt32(result);
+
             }
-            return value;
+            else
+            {
+                if (Int32.TryParse(list[0], out int result))
+                    return Convert.ToInt32(list[0]) + CalSum(list.Skip(1).ToList());
+            }
+            return CalSum(list.Skip(1).ToList());
         }
-
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var testarray = new string[] { "okay", "null", "Tlholo", "6", "lol", "4", "six", "happy", "9" };
+            var list2 = new List<string>() {  "100", "geek", "200", "400", "for", "101", "2018", "64", "74", "geeks", "27", "7810"};
+            Console.WriteLine($"Sum of Intergers in String List{CalSum(list2)}" ); 
 
-            Console.WriteLine("Sum of integers in string array {0}", GetsumIntArray(testarray));
+
             Console.ReadKey();
-
         }
     }
-
 }
+
